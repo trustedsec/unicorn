d85232b1cb3a097f03fec192a813f0ea17ed4744
TrustedSec <info@trustedsec.com>
2018-09-07 19:39:56 -0400

    bug fix

--- unicorn.py
+++ unicorn.py
@@ -429,7 +429,7 @@ python unicorn.py ms
 
 # usage banner
 def gen_usage():
-    print("-------------------- Magic Unicorn Attack Vector v3.2.8 -----------------------------")
+    print("-------------------- Magic Unicorn Attack Vector v3.2.9 -----------------------------")
     print("\nNative x86 powershell injection attacks on any Windows platform.")
     print("Written by: Dave Kennedy at TrustedSec (https://www.trustedsec.com)")
     print("Twitter: @TrustedSec, @HackingDave")
