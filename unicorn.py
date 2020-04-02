f37315556de7f8543f24f9fd2b0f999012078adc
David Kennedy <david.kennedy@trustedsec.com>
2020-04-01 21:26:27 -0400

    update version of unicorn

--- unicorn.py
+++ unicorn.py
@@ -495,7 +495,7 @@ python unicorn.py ms
 
 # usage banner
 def gen_usage():
-    print("-------------------- Magic Unicorn Attack Vector v3.8.6 -----------------------------")
+    print("-------------------- Magic Unicorn Attack Vector v3.8.7 -----------------------------")
     print("\nNative x86 powershell injection attacks on any Windows platform.")
     print("Written by: Dave Kennedy at TrustedSec (https://www.trustedsec.com)")
     print("Twitter: @TrustedSec, @HackingDave")
